8f9a99493910abb898c993c48aa882655a856d54
Jesserey Joseph <3p.ibm.jjoseph-dev@psslaidev.com>
2025-10-21 20:30:03 +0800

    Update Matching on LTL

--- Muda/Matching.v
+++ Muda/Matching.v
@@ -15,6 +15,96 @@ Proof. intros; unfold residual_bid; lia. Qed.
 Lemma residual_ask_ge_0 : forall a ms, 0 <= residual_ask a ms.
 Proof. intros; unfold residual_ask; lia. Qed.
 
+Require Import Coq.Lists.List.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.PeanoNat.
+Require Import Coq.micromega.Lia.
+Require Import MUDA.MUDA.Types.
+Require Import MUDA.MUDA.State.
+Import ListNotations.
+
+(** ------------------------------------------------------------------ **)
+(** Feasible-pair search (must come BEFORE lemmas that use it)          **)
+(** ------------------------------------------------------------------ **)
+
+Fixpoint find_feasible (bs : list Bid) (as_list : list Ask) (ms : list Match)
+  : option (Bid * Ask) :=
+  match bs with
+  | [] => None
+  | b :: bs' =>
+      match as_list with
+      | [] => None
+      | a :: as' =>
+          (* price guard *)
+          if Nat.leb (ask_price a) (price b) then
+            (* positive residuals guard *)
+            if (Nat.leb 1 (residual_bid b ms)) && (Nat.leb 1 (residual_ask a ms))
+            then Some (b, a)
+            else find_feasible bs' as_list ms
+          else None
+      end
+  end.
+
+
+Lemma andb_true_iff : forall x y, x && y = true <-> x = true /\ y = true.
+Proof. intros [] []; simpl; split; auto; discriminate. Qed.
+
+(** The precise specification of the guards that must have succeeded
+    whenever [find_feasible] returns [Some (b,a)]. *)
+Lemma find_feasible_spec :
+  forall bs as_list ms b a,
+    find_feasible bs as_list ms = Some (b,a) ->
+    ask_price a <= price b /\
+    1 <= residual_bid b ms /\
+    1 <= residual_ask a ms.
+Proof.
+  induction bs as [|b0 bs IH]; intros as_list ms b a H; simpl in H.
+  - discriminate.
+  - destruct as_list as [|a0 as']; simpl in H; [discriminate|].
+    destruct (Nat.leb (ask_price a0) (price b0)) eqn:Hprice.
+    + apply Nat.leb_le in Hprice.
+      destruct ((Nat.leb 1 (residual_bid b0 ms)) &&
+                (Nat.leb 1 (residual_ask a0 ms))) eqn:Hsuf.
+      * inversion H; subst b a b0 a0; clear H.
+        apply andb_true_iff in Hsuf as [Hb Ha].
+        apply Nat.leb_le in Hb.
+        apply Nat.leb_le in Ha.
+        repeat split; auto.
+      * (* fell through to the recursive call on (bs, as_list) *)
+        eapply IH in H; eauto.
+    + discriminate.
+Qed.
+
+(** ------------------------------------------------------------------ **)
+(** Your original create_match stays as you had it                      **)
+(** ------------------------------------------------------------------ **)
+
+Definition create_match (b : Bid) (a : Ask) (ms : list Match) : Match :=
+  let q := Nat.min (residual_bid b ms) (residual_ask a ms) in
+  {| matched_bid := b;
+     matched_ask := a;
+     match_quantity := q |}.
+
+(** A tiny arithmetic helper. *)
+Lemma min_pos_from_le1 :
+  forall x y, 1 <= x -> 1 <= y -> 0 < Nat.min x y.
+Proof. intros x y Hx Hy. destruct (le_lt_dec x y); lia. Qed.
+
+(** One match consumes strictly positive quantity. *)
+Lemma match_quantity_pos :
+  forall s b a,
+    find_feasible (bids s) (asks s) (matches s) = Some (b,a) ->
+    0 < match_quantity (create_match b a (matches s)).
+Proof.
+  intros s b a Hf.
+  unfold create_match; simpl.
+  pose proof (find_feasible_spec (bids s) (asks s) (matches s) b a Hf)
+    as [_ [Hb Ha]].
+  apply min_pos_from_le1; lia.
+Qed.
+
+
+
 (* One match consumes strictly positive quantity *)
 Lemma match_quantity_pos :
   forall s b a,
